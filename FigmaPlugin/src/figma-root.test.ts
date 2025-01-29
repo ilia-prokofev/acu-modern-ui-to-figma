df7e7fd00f927e74d4bea7b40433dffcad81c3fd
Nikita Popov <Nikita.Popov@acumatica.com>
2025-01-29 11:57:41 +0100

    tests

---
+++ FigmaPlugin/src/figma-root.test.ts
@@ -0,0 +1,31 @@
+import * as fs from "node:fs";
+import {Root} from "./elements/qp-root";
+
+import {figmaRoot} from "./figma-root";
+
+describe('figma-root', () => {
+    interface testCase {
+        testName: string;
+        incomingJSONFileName: string;
+        expectedJSONFileName: string;
+    }
+    const testCases: testCase[] = [
+        {
+            testName: "SC301000",
+            incomingJSONFileName: './test-cases/SC301000-input.json',
+            expectedJSONFileName: './test-cases/SC301000-output.json',
+        }]
+
+    it.each(testCases)(
+        '$testName',
+        async ({incomingJSONFileName, expectedJSONFileName}) => {
+            const incomingJSON = fs.readFileSync(incomingJSONFileName, 'utf8');
+            const expectedJSON = fs.readFileSync(expectedJSONFileName, 'utf8');
+
+            const root = JSON.parse(incomingJSON) as Root;
+            const actual = new figmaRoot(root);
+            const expected = JSON.parse(expectedJSON);
+            expect(actual).toEqual(expected);
+        }
+    );
+})
