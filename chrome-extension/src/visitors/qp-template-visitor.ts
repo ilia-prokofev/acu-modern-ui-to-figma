dce346089d896e489c9bd9e06759b143c638861e
Nikita Popov <Nikita.Popov@acumatica.com>
2025-10-21 11:07:49 +0200

    qp-template class parsing added

--- chrome-extension/src/visitors/qp-template-visitor.ts
+++ chrome-extension/src/visitors/qp-template-visitor.ts
@@ -1,9 +1,9 @@
-import { AcuElement, AcuElementType } from '@modern-ui-to-figma/elements';
+import {AcuElement, AcuElementType, LabelLength, QPFieldsetStyle} from '@modern-ui-to-figma/elements';
 import { AcuContainer } from '@modern-ui-to-figma/elements';
 import { Template } from '@modern-ui-to-figma/elements';
 import ElementVisitor from './qp-element-visitor';
 import ChildrenVisitor from './children-visitors';
-import { concatElementID } from './html-element-utils';
+import {concatElementID, findClasses} from './html-element-utils';
 
 export default class QPTemplateVisitor implements ElementVisitor {
     constructor(private readonly childrenVisitor: ChildrenVisitor) {}
@@ -22,10 +22,28 @@ export default class QPTemplateVisitor implements ElementVisitor {
             Id: concatElementID(parent.Id, htmlElement),
             Name: htmlElement.attributes.getNamedItem('name')?.value ?? null,
             Children: [],
+            LabelLength: this.parseTemplateStyle(htmlElement),
         };
 
         (parent as AcuContainer).Children.push(child);
         this.childrenVisitor.visitChildren(htmlElement, child);
         return true;
     }
+
+    private parseTemplateStyle(element: Element): LabelLength {
+        if (findClasses(element, 'label-size-xm')) {
+            return LabelLength.xm;
+        }
+
+        if (findClasses(element, 'label-size-m')) {
+            return LabelLength.m;
+        }
+
+        if (findClasses(element, 'label-size-sm')) {
+            return LabelLength.sm;
+        }
+
+        return LabelLength.s;
+    }
+
 }
--- elements/src/qp-template.ts
+++ elements/src/qp-template.ts
@@ -1,7 +1,15 @@
 import {AcuContainer} from './acu-container';
 import {AcuElementType} from './acu-element';
 
+export enum LabelLength {
+    s = 's',
+    sm = 'sm',
+    m = 'm',
+    xm = 'xm',
+}
+
 export interface Template extends AcuContainer {
     Type: AcuElementType.Template;
     Name: string | null;
+	LabelLength: LabelLength;
 }
